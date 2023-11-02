34c0365022792118da7defb74c2ce8776e5a91e1
Tiger Liu <tigerliu@pm.me>
2023-11-02 00:54:35 -0400

    Added a window.onload function to save API key for first time visiter, so the app now requires user's own API key. Plus an working search feature, although I still need to make it display a default city or something.

--- script.js
+++ script.js
@@ -2,15 +2,41 @@
 
 // Since no back end so I probably will store it in a json file and have users enter their own api key.
 
-const api = ""; // Don't store the api key in this file bro
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=boston";
+window.onload = function () {
+  let apiKey = localStorage.getItem("weatherApiKey");
 
-async function checkWeather() {
-  const result = await fetch(apiUrl + `&appid=${api}`);
+  if (!apiKey) {
+    // API key not found, show a popup to ask for it
+    apiKey = prompt("Please enter your API key:");
+
+    if (apiKey) {
+      // Save the entered API key to localStorage
+      localStorage.setItem("weatherApiKey", apiKey);
+    }
+  }
+};
+
+const api = localStorage.getItem("weatherApiKey");
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const search = document.querySelector(".search input");
+const searchbttn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const result = await fetch(apiUrl + city + `&appid=${api}`);
   var weatherData = await result.json();
 
   console.log(weatherData);
+
+  document.querySelector(".city").textContent = weatherData.name;
+  document.querySelector(".temp").textContent =
+    Math.round(weatherData.main.temp) + "°C";
+  document.querySelector(".wind").innerHTML = weatherData.wind.speed + "km/h";
+  document.querySelector(".humidity").textContent =
+    weatherData.main.humidity + "%";
 }
 
-checkWeather();
+searchbttn.addEventListener("click", () => {
+  checkWeather(search.value);
+});
